4b5f57548a16883c9f96b35af4e50017aaa4dae1
Roderick Lagunas <rlagunas@fidelizador.com>
2017-05-03 16:40:48 -0300

    mejora de autoload

--- lib/autoload.php
+++ lib/autoload.php
@@ -16,22 +16,3 @@ spl_autoload_register(function($element){
         include_once($filename);
     }
 });
-/*
-spl_autoload_register(function ($lib) {
-    $classname = str_replace('MVC\\','',$lib);
-    $filename = "../lib/". $classname .".php";
-    include_once($filename);
-});
-
-spl_autoload_register(function ($controller) {
-    list($module, $controller) = explode('\\',$controller);
-    $filename = "../controller/$module/$controller.php";
-    include_once($filename);
-});
-
-spl_autoload_register(function ($model) {
-    $classname = str_replace('Model\\','',$model);
-    $filename = "../model/". $classname .".php";
-    include_once($filename);
-});
-*/
