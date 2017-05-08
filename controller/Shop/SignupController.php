5d890ce73b0d9f9b682bd35b7f7ce63fe93d9725
Ascariel <pablocangas@gmail.com>
2017-05-07 22:32:37 -0300

    navbar basico, guardando los datos del signup form en session

--- controller/Shop/SignupController.php
+++ controller/Shop/SignupController.php
@@ -4,15 +4,18 @@ namespace Controller\Shop;
 use Model\User;
 
 class SignupController {
-    #static $template = 'Layout/base.html.php';
+    static $template = 'Layout/base.html.php';
 
     function indexAction() {
-
+    	return ['title'=> "Registro Usuarios"];
     }
 
 
-    function signupUserAction(){
-      
+    function userSignupAction(){
+      $_SESSION["nombre"] = $_POST["nombre"];
+      $_SESSION["apellido"] = $_POST["apellido"];
+      $_SESSION["email"] = $_POST["email"];      
+
       return [];
     }
 }
--- view/Shop/Signup/index.html.php
+++ view/Shop/Signup/index.html.php
@@ -1,56 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registo Usuarios</title>
-  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-</head>
-<body>
 
+<br><br>
+<div class="container">
 
-  
-  <br><br>
-  <div class="container">
-
-    <div class="row">
-      <div class="col-md-6 col-md-offset-3">
-        <h1 class="text-center">Registrar Usuarios</h1>
-        <br>  
-              
-        <form action="/registrar_usuario.php" method="POST">
-          <div class="form-group">
-            Nombre <input type="text" name="" class="form-control ">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <h1 class="text-center">Registrar Usuarios</h1>
+      <br>  
+            
+      <form action="/shop/signup/userSignup" method="POST">
+        <div class="form-group">
+          Nombre <input type="text" required name="nombre" class="form-control ">
+        </div>
+        <div class="form-group">
+          Apellido <input type="text" required name="apellido" class="form-control">
+        </div>
+        <div class="form-group">
+          Email <input type="text" required name="email" class="form-control">
+        </div>
+                            
+        
+        <br>
+        <div class="row">
+          <div class="col-md-6">
+            <button class="btn btn-success btn-block">Registrar Usuario</button>
           </div>
-          <div class="form-group">
-            Apellido <input type="text" name="" class="form-control">
+          <div class="col-md-6">
+            <a class="btn btn-default btn-block" href="/">Volver</a>
           </div>
-          <div class="form-group">
-            Email <input type="text" name="" class="form-control">
-          </div>
-          <div class="form-group">
-            <select class="form-control">
-              <option class="form-control" value="admin">Admin</option>
-              <option class="form-control" value="cliente">Cliente</option>
-            </select>
-          </div>
-                              
-          
-          <br>
-          <div class="row">
-            <div class="col-md-6">
-              <button class="btn btn-success btn-block">Registrar Usuario</button>
-            </div>
-            <div class="col-md-6">
-              <button class="btn btn-default btn-block">Volver</button>
-            </div>
-          </div>
-          
-      
-        </form>
-      </div>
+        </div>
+        
+    
+      </form>
     </div>
-
   </div>
 
+</div>
 
-</body>
-</html>
