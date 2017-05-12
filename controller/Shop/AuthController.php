1b1eed42fd057497a6a2050401ed0e7821295a57
Roderick Lagunas <rlagunas@fidelizador.com>
2017-05-12 20:17:18 -0300

    autenticacion y registro de usuario

--- controller/Shop/AuthController.php
+++ controller/Shop/AuthController.php
@@ -27,19 +27,55 @@ class AuthController extends Controller {
     }
 
     function doSignupAction(){
-        if(empty($_SESSION['user'])) $_SESSION['user'] = $_POST;
-        $_SESSION['user']['id']=1;
+        $auth = $this->post('auth');
+        $email = str_replace("'","\'", $auth['email']);
+        $exists = $this->userExists($email);
+
+        if(!empty($exists)) {
+            $_SESSION['message']="Usurio o password invalidos";
+            $this->redirect('/auth/signup');
+        }
+        $exists = [
+            'email'=>$email,
+            'password'=>sha1($auth['password']),
+            'is_admin'=>false,
+            'vcard'=>null,
+            'address'=>null,
+        ];
+        $user = new User;
+        $user->create($exists);
+        $exists['id']=$user->getLastId();
+        $_SESSION['user'] = $exists;
+
         $this->redirect('/shop/product');
     }
 
     function doSigninAction(){
-      if(empty($_SESSION['user'])) $_SESSION['user'] = $_POST;
-      $_SESSION['user']['id']=1;
-      $this->redirect('/shop/product');
+        $auth = $this->post('auth');
+        $email = str_replace("'","\'", $auth['email']);
+        $pwd = $auth['password'];
+        $exists = $this->userExists($email);
+
+        if((bool)(int)$exists['is_admin']){
+            $_SESSION['message']='Usuario o password invalido!';
+            unset($exists);
+            $this->redirect('/shop/auth/signin');
+        }
+        if($exists['password'] !== sha1($pwd)){
+            $_SESSION['message']='Usuario o password invalido!';
+            unset($exists);
+            $this->redirect('/shop/auth/signin');
+        }
+        $_SESSION['user'] = $exists;
+        $this->redirect('/shop/product');
     }
 
     function logoutAction(){
       unset($_SESSION['user']);
       $this->redirect('/shop/product');
+    }
+
+    private function userExists($email){
+        return (new User)->select(['id','password','email', 'fullname', 'is_admin'], "email='$email'")->fetch();
     }
 }
--- view/Shop/Auth/signin.html.php
+++ view/Shop/Auth/signin.html.php
@@ -1,1 +1,2 @@
+<?php $signin = true; ?>
 <?php include('signup.html.php') ?>
