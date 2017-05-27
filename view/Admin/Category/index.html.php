950690272471f66ea514426cb8cea2340effe491
Estefania <estefania.rosales.s@gmail.com>
2017-05-27 10:46:27 -0400

    h2 agregado

--- view/Admin/Category/index.html.php
+++ view/Admin/Category/index.html.php
@@ -5,6 +5,7 @@
 
 		<h1 class=""><?php echo $title ?></h1><br>
 	 	<a href="/admin/category/new" class="btn btn-sm btn-info">Crear Nueva Categoria</a>
+	 	<h2>Hola!</h2>
 
 	 	<table class="data-table table table-hover table-striped table-condensed ">
 	 		<thead >
